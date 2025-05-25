7f04d756e9a1efcee33ecb925c8f57e0bc21dad2
RahmDridiCode <dridirahma951@gmail.com>
2025-05-25 16:27:33 +0100

    faire les fonction rotate les filtres et appload

---
+++ app.py
@@ -0,0 +1,120 @@
+from flask import Flask, render_template, request, jsonify,send_file,make_response
+from PIL import Image, ImageOps, ImageFilter
+import os
+from io import BytesIO
+from mimetype import mime_types
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    file = request.files['image']
+
+    if file:
+        original_filename = file.filename
+        name, extension = os.path.splitext(original_filename)  # name = "photo", extension = ".png"
+        path = os.path.join(app.config['UPLOAD_FOLDER'], original_filename)
+        file.save(path)
+        mimeType = get_mime_type_from_extension(extension)
+
+        return jsonify({'path': path, 'filename_with_ext': original_filename, 'extention':extension, "mime_type":mimeType})
+    return jsonify({'error': 'No image uploaded'}), 400
+
+
+def prepare_image_response(image, extension, fileName):
+    save_format = extension.replace('.', '').upper()
+    if save_format == 'JPG':
+        save_format = 'JPEG'
+
+    img_io = BytesIO()
+    image.save(img_io, format=save_format)
+    img_io.seek(0)
+
+    mimetype = get_mime_type_from_extension(extension)
+
+    response = make_response(send_file(
+        img_io,
+        mimetype=mimetype,
+        as_attachment=False,
+        download_name=fileName + extension
+    ))
+    response.headers['X-Image-Extension'] = extension
+    response.headers['X-Image-fileName'] = fileName
+    return response
+
+
+def get_image_from_request():
+    file = request.files.get('image')
+    extension = request.form.get('extension')
+    fileName = request.form.get('fileName')
+
+    if not file:
+        return None, None, None, "Image manquante", 400
+
+    try:
+        image = Image.open(file.stream)
+    except Exception as e:
+        return None, None, None, f"Erreur lors du chargement de l'image : {e}", 400
+
+    return image, extension, fileName, None, 200
+
+
+@app.route('/process/gray', methods=['POST'])
+def process_greyscale():
+    image, extension, fileName, error, status = get_image_from_request()
+    if error:
+        return error, status
+
+    image = ImageOps.grayscale(image)
+    return prepare_image_response(image, extension, fileName)
+
+
+@app.route('/process/rotate', methods=['POST'])
+def process_rotate():
+    image, extension, fileName, error, status = get_image_from_request()
+    if error:
+        return error, status
+
+    image = image.rotate(-90, expand=True)
+    return prepare_image_response(image, extension, fileName)
+
+
+
+
+
+@app.route('/process/filtre', methods=['POST'])
+def filter_image():
+    image, extension, fileName, error, status = get_image_from_request()
+    typeFiltre = request.form.get('typeFiltre')
+    if error:
+        return error, status
+    if typeFiltre == "invert":
+        image = ImageOps.invert(image.convert("RGB"))
+    
+    if typeFiltre == "blur":
+        image = image.filter(ImageFilter.BLUR)
+    if typeFiltre == "sharpen":
+        image = image.filter(ImageFilter.SHARPEN)
+    if typeFiltre == "contour":
+        image = image.filter(ImageFilter.CONTOUR)
+
+
+
+    return prepare_image_response(image, extension, fileName)
+
+
+
+def get_mime_type_from_extension(extension):
+    return mime_types.get(extension.lower(), 'application/octet-stream')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,106 @@
+let pathOriginalImage
+let mimeType
+let originalExtension = null;
+let originalFileName = null;
+
+let scale = 1;
+let rotation = 0;
+
+let currentScale = 1;
+const imageElement = document.getElementById('image');
+const imageLabel =document.getElementById('imageNameSpan');
+document.getElementById('upload').addEventListener('change', function () {
+    const formData = new FormData();
+    formData.append("image", this.files[0]);
+
+    fetch('/upload', {
+        method: 'POST',
+        body: formData
+    })
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById('image').src = data.path + '?v=' + new Date().getTime();
+            document.getElementById('imageNameSpan').textContent = data.filename_with_ext;
+            document.querySelectorAll('button, select').forEach(el => el.disabled = false);
+            pathOriginalImage = data.path
+            mimeType = data.mime_type
+            originalFileName = data.filename
+            originalExtension = data.extention
+            imageElement.textContent = originalFileName;
+        });
+});
+
+
+
+function reset(){
+    imageElement.src = pathOriginalImage;
+}
+
+
+async function process(action) {
+    const typeFiltre = document.getElementById('filterSelect').value; 
+    const isBlob = imageElement.src.startsWith("blob:");
+
+
+    // Créer le canvas
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+
+    img.onload = async function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+
+        // Définir l'extension et le nom de fichier si c’est le premier traitement
+
+        if (!mimeType) {
+            alert("Format non supporté !");
+            return;
+        }
+
+        canvas.toBlob(async function (blob) {
+            const formData = new FormData();
+            formData.append('image', blob, 'image' + originalExtension);
+            formData.append('action', action);
+            formData.append('extension', originalExtension);
+            formData.append('fileName', originalFileName);
+            formData.append('pathOriginalImage', pathOriginalImage);
+            formData.append('typeFiltre', typeFiltre);
+
+            const response = await fetch(`/process/${action}`, {
+                method: 'POST',
+                body: formData
+            });
+
+            const resultBlob = await response.blob();
+            imageElement.src = URL.createObjectURL(resultBlob);
+        }, mimeType);
+    };
+
+    img.src = imageElement.src;
+}
+
+
+function zoomIn() {
+    currentScale += 0.1; // augmenter le zoom
+    updateImageScale();
+}
+
+function zoomOut() {
+    currentScale = Math.max(0.1, currentScale - 0.1); // diminuer mais pas en dessous de 0.1
+    updateImageScale();
+}
+
+function updateImageScale() {
+    imageElement.style.transform = `scale(${currentScale})`;
+    imageElement.style.transition = 'transform 0.3s ease'; // animation fluide
+}
+
+/*
+function changeExtention(){
+    const returnedExtension = response.headers.get('X-Image-Extension');
+    const returnedFileName = response.headers.get('X-Image-fileName');
+    document.getElementById('imageNameSpan').textContent = returnedFileName + returnedExtension;
+}*/
